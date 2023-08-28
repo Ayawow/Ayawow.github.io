4855abca6349c4c735443bb3d76cb7c463a65781
Ayawow <1123325454@qq.com>
2023-08-28 14:39:04 +0800

    change

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
@@ -33,39 +33,3 @@ if (!localStorage.getItem("name")) {
 myButton.onclick = function () {
     setUserName();
 };
-=======
-let myImage = document.querySelector("img");
-
-myImage.onclick = function () {
-    let mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/1.jpg") {
-        myImage.setAttribute("src", "images/1.png");
-    } else {
-        myImage.setAttribute("src", "images/1.jpg");
-    }
-};
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUserName() {
-    let myName = prompt("请输入你的名字。");
-    if (!myName) {
-        setUserName();
-    } else {
-        localStorage.setItem("name", myName);
-        myHeading.textContent = "Mozilla 酷毙了，" + myName;
-    }
-}
-
-if (!localStorage.getItem("name")) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
-}
-
-myButton.onclick = function () {
-    setUserName();
-};
->>>>>>> 6ec8e23 (第一次提交)
